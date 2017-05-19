471c0211ef82a855ab2a255cb6d409184060b6fe
Alyssa Pratt <snubly1@gmail.com>
2017-05-19 14:21:25 -0400

    normalized year being passed in as string

--- lib/user_importer.rb
+++ lib/user_importer.rb
@@ -1,7 +1,32 @@
 class UserImporter
+  attr_reader :filename
 
   def initialize(filename)
     @filename = filename
+
+    @table = RemoteTable.new "#{@filename}"
+  end
+
+  def rows
+    @table.rows
+  end
+
+  def normalize_year(years)
+    list_of_years = years.split(/[,&]/)
+    list_of_years.map do |year|
+      year = year.strip
+      if year.length == 2
+        "20#{year}"
+      else
+        year
+      end
+    end
   end
 
 end
+# last name e.rows.first["Last Name"]
+# some stuff with names e.rows.first["Title Name"] maybe title or first name or last name or whatever
+# email e.rows.first["Email"] but sometimes 2 emails
+# year e.rows.first["Year"]
+# position e.rows.first["Position"]
+# e.rows.first["Additional Info"]
--- spec/lib/user_importer_spec.rb
+++ spec/lib/user_importer_spec.rb
@@ -7,4 +7,33 @@ describe 'UserImporter' do
   it 'instantiates with a filename' do
     expect { subject }.to_not raise_error
   end
+
+  it 'can read the file' do
+    expect(subject.filename).to eq(filename)
+  end
+
+  it 'read the first row' do
+    expect(subject.rows.first).to eq({
+                                      "Last Name"=>"zzzztt",
+                                      "Title Name"=>"zz. zzzzy zzzztt",
+                                      "Email"=>"zzzzztt86@gzzzz.zzz",
+                                      "Year "=>"2008,09",
+                                      "Position "=>"Outdoor Extreme",
+                                      "Additional Info"=>""
+                                      })
+    end
+
+  context 'can sanitize the data' do
+    it 'splits the year' do
+      expect(subject.normalize_year("2016,14,17")).to eq(["2016", "2014", "2017"])
+    end
+
+    it 'handles whitespace correctly' do
+      expect(subject.normalize_year("1234,    1123,  45, 34   ,14")).to eq(["1234", "1123", "2045", "2034", "2014"])
+    end
+
+    it 'handles & correctly' do
+      expect(subject.normalize_year("  2016 &     14,   17")).to eq(["2016", "2014", "2017"])
+    end
+  end
 end
