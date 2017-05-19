16deed9e2b663861e009f76dc6fa023d2db8242c
Greg <gregory.kenenitz@gmail.com>
2017-05-19 15:41:57 -0400

    Creates :email_templates table

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170519141609) do
+ActiveRecord::Schema.define(version: 20170519193625) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "hstore"
+
+  create_table "email_templates", force: :cascade do |t|
+    t.string "label"
+    t.string "subject"
+    t.string "body"
+    t.integer "author_id"
+    t.index ["author_id"], name: "index_email_templates_on_author_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
