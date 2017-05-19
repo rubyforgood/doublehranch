a33e16ca1fe5be582ceb79a9dccfa93b3cb805b5
Josh Langholtz <jjlangholtz@gmail.com>
2017-05-19 18:45:23 -0400

    Switch to bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  root to: 'home#index'
+
+  get '/verification', to: 'verification#show'
 
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  root to: "home#index"
-
   resources :posts
-
 end
