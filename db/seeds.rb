efcf6d3b2cb9d17d90b471d60f4f2760417c3868
arbianchi <adina.r.bianchi@gmail.com>
2017-05-19 16:24:58 -0400

    Set up position seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,4 +14,11 @@ unless Rails.env.test?
   100.times do
     FactoryGirl.create(:user)
   end
+  
+  position_names = ["Staff Member", "Camper", "Counselor", "Volunteer"]
+  position_types = ["ALCOA", "Activities Coordinator", "Aquatics Director", "Archery", "Arts & Crafts", "Arts & Crafts Assistant", "Arts & Crafts Director", "Barn", "Barn Director", "Barn Manager", "Barn Staff", "CIT", "CIT/LIT Associate", "Counselor", "Creative Arts", "Creative Arts - Media Specialist", "Creative Arts - Photo/News", "Creative Arts - Photographer", "Dietary Intern", "Discovery", "High Ropes Assistant", "Housekeeping", "LIT", "Leadership Coordinator", "Lifeguard", "Medical Student", "Nurse (Non-Residential)", "Outdoor Extreme", "Photographer", "Programs Associate", "Psych Intern", "Residential Life Coordinator", "Ropes Course", "Ropes Course Assistant", "Ropes Director", "Security", "Team Adventure", "Unit Leader", "Volunteer Associate"]
+  
+  User.where(admin: true).each do |admin|
+    # TODO: Give admin users a position
+  end
 end
