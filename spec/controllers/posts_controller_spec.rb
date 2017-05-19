59039eba6d2c62d0dfc1162bd8d25865200ad94d
DIANA JURGIELEWICZ <DMJURG@GMAIL.com>
2017-05-19 18:16:13 -0400

    adds post controller test

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -2,4 +2,20 @@ require 'rails_helper'
 
 RSpec.describe PostsController, type: :controller do
 
+  let(:user) { create(:user) }
+
+  describe "POST create" do
+    context "with valid attributes" do
+      before do
+        user.confirm
+        sign_in user
+      end
+
+      it "creates a post with an associated user" do
+        post :create, params: {post: {caption: "Here is my first post"}}
+        expect(Post.last.user).to eq user
+      end
+    end
+  end
+
 end
