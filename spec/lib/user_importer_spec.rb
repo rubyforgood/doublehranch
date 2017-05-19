3ae576138dd90ea2d76bfd9f582ce1314e1169c6
maebeale <maebeale@gmail.com>
2017-05-19 11:57:17 -0400

    Add filename & subject to let blocks in importer spec

--- spec/lib/user_importer_spec.rb
+++ spec/lib/user_importer_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
 describe 'UserImporter' do
-
+  let(:filename) { File.join(Rails.root, "spec", "resources", "anonymized_user_data.xlsx") }
+  let(:subject) { UserImporter.new(filename) }
 
   it 'instantiates with a filename' do
-    filename = File.join(Rails.root, "spec", "resources", "anonymized_user_data.xlsx")
-    expect { UserImporter.new(filename) }.to_not raise_error
+    expect { subject }.to_not raise_error
   end
 end
