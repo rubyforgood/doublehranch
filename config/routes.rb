f597189190e4771e06e6e21395d27bc7bad9a955
arbianchi <adina.r.bianchi@gmail.com>
2017-05-19 18:58:58 -0400

    clean up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  root to: 'home#index'
-
-  devise_for :users
-  
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   root to: "home#index"
   
   get '/verification', to: 'verification#show'
