c25a6644351b07d6b9f557892b498f12b9b56a9a
arbianchi <adina.r.bianchi@gmail.com>
2017-05-19 18:47:27 -0400

    update seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-binding.pry
-
 unless Rails.env.test?
   10.times do
     user = FactoryGirl.create(:user, :admin)
