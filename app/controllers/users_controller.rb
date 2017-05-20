464b1ee19e6dc3a60fc9ac44e1cc08eec6fc9ff2
arbianchi <adina.r.bianchi@gmail.com>
2017-05-20 10:38:13 -0400

    Create user directory page.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,15 @@ class UsersController < ApplicationController
 
   before_action :authenticate_user!
 
+  def index
+    @users = User.all
+  end
+
   def show
+    @user = current_user
+  end
+  
+  def edit
     @user = current_user
   end
 end
