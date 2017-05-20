d4f32bd690bbb235ba60b9a2eaa33b69ba363b83
Josh Langholtz <jjlangholtz@gmail.com>
2017-05-20 12:59:25 -0400

    Add initial verification#update logic

--- app/controllers/verification_controller.rb
+++ app/controllers/verification_controller.rb
@@ -7,5 +7,30 @@ class VerificationController < ApplicationController
   end
 
   def update
+    user = User.find(params[:user_id])
+
+    if user.update(verification_params)
+      redirect_to new_user_session_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def verification_params
+    permitted_params = %i(
+      city
+      email
+      first_name
+      last_name
+      maiden_name
+      salutation
+      state
+      street_address
+      zip_code
+    )
+
+    params.require(:user).permit(*permitted_params)
   end
 end
