2e262f3d769c44239434fc88377db67a4db4e07a
arbianchi <adina.r.bianchi@gmail.com>
2017-05-20 14:04:36 -0400

    Add update user method.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,8 +9,23 @@ class UsersController < ApplicationController
   def show
     @user = current_user
   end
-  
+
   def edit
     @user = current_user
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      redirect_to users_path, notice: 'User updated.'
+    else
+      redirect_to users_path, alert: 'Unable to update user.'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :street_address, :city, :state, :country, :zip_code, :maiden_name, :salutation, :privacy_settings, :email, :password, :phone, :facebook_username, :twitter_handle, :instagram_username, :tumblr_name, :linkedin_username, :pinterest_name, :profile_photo_file_size, :profile_photo_file_name, :profile_photo_content_type)
   end
 end
