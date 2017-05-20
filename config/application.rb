a14add9d91e80c9a8bade9ba20f9cf2381848732
Marliana Lara <marliana.lara@gmail.com>
2017-05-20 10:26:34 -0400

    Add font and media query variables

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module DoubleHRanch
     config.load_defaults 5.1
     config.autoload_paths += Dir["#{config.root}/lib/**/"]
     config.assets.paths << Rails.root.join("node_modules")
+    config.assets.paths << Rails.root.join('app/assets/fonts')
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
