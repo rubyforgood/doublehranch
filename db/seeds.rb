147daa605027b9d08b45f10edb3081f937077c9e
Greg <gregory.kenenitz@gmail.com>
2017-05-20 13:47:31 -0400

    fixes seeds typo w/r/t tags, adds some sample admin users to db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,12 @@
 unless Rails.env.test?
+  %w(info help).each do |email|
+    User.create(
+      admin: true,
+      email: "#{email}@doublehranch.com",
+      password: 'test-admin-accounts'
+    )
+  end
+
   10.times do
     user = FactoryGirl.create(:user, :admin)
     user.confirm
@@ -9,7 +17,7 @@ unless Rails.env.test?
     admin.confirm
   end
   
-  post = Post.create(captions: "My first post!", user_id: User.first)
-  post.tag_last.add('tags', 'more tags')
+  post = Post.create(caption: "My first post!", user_id: User.first)
+  post.tag_list.add('tags', 'more tags')
   
 end
---
+++ spec/controllers/sent_emails_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe SentEmailsController, type: :controller do
+
+end
