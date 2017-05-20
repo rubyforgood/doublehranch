a75173f2de69a7160a2534c1a00cc2d09677be8b
arbianchi <adina.r.bianchi@gmail.com>
2017-05-19 20:02:18 -0400

    update seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,15 @@
-# unless Rails.env.test?
-#   10.times do
-#     user = FactoryGirl.create(:user, :admin)
-#     user.confirm
-#   end
-# 
-#   100.times do
-#     admin = FactoryGirl.create(:user)
-#     admin.confirm
-#   end
-# end
-binding.pry
+unless Rails.env.test?
+  10.times do
+    user = FactoryGirl.create(:user, :admin)
+    user.confirm
+  end
+
+  100.times do
+    admin = FactoryGirl.create(:user)
+    admin.confirm
+  end
+  
+  post = Post.create(captions: "My first post!", user_id: User.first)
+  post.tag_last.add('tags', 'more tags')
+  
+end
