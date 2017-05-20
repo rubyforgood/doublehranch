9141d4987429a664b8daac93e0dba990e493d5fd
DIANA JURGIELEWICZ <DMJURG@GMAIL.com>
2017-05-20 10:47:48 -0400

    adds comments form and creates controller and routes for comments

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,6 +19,19 @@ class PostsController < ApplicationController
     end
   end
 
+  def comment
+    @comment = Comment.new(comment_params)
+    @comment.user = current_user
+    @comment.commentable_id = params[:post_id]
+    @comment.commentable_type = "Post"
+
+    if @comment.save
+      redirect_to post_path(@comment.commentable_id), notice: "Comment was successfully created"
+    else
+      render post_path(@comment.commentable_id)
+    end
+  end
+
   def show
     @post = Post.includes(comments: [:user]).find(params[:id])
     @comment = @post.comments.new(user: current_user)
@@ -32,6 +45,10 @@ class PostsController < ApplicationController
 
   def post_params
     params.require(:post).permit(:caption, :photo)
+  end
+
+  def comment_params
+    params.require(:comment).permit(:comment, :commentable_type, :commentable_id)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   root to: "home#index"
-  
+
   get '/verification', to: 'verification#show'
-  
-  get "/profile/:user_id", to: "users#show", as: 'user_profile'
+
+  get '/profile/:user_id', to: 'users#show', as: 'user_profile'
+
+  post '/posts/:post_id/comment', to: 'posts#comment', as: 'comment_post'
 
   devise_for :users
   resources :posts
+  resources :comments, only: [:update, :destroy]
+
 end
