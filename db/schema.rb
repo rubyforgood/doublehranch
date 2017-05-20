a79982c7d1550d08cb5d33462f1be9fc6359203d
Greg <gregory.kenenitz@gmail.com>
2017-05-20 11:03:05 -0400

    re-checks in schema after running migrations & having table names alphabetized post-merge conflict

--- db/schema.rb
+++ db/schema.rb
@@ -10,22 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170520133954) do
+ActiveRecord::Schema.define(version: 20170520142409) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "hstore"
-
-  create_table "email_templates", force: :cascade do |t|
-    t.string "label"
-    t.string "subject"
-    t.string "body"
-    t.integer "author_id"
-    t.index ["author_id"], name: "index_email_templates_on_author_id"
-
-  create_table "positions", force: :cascade do |t|
-    t.string "name"
-    t.string "position_type"
 
   create_table "comments", id: :serial, force: :cascade do |t|
     t.string "title", limit: 50, default: ""
@@ -41,6 +30,14 @@ ActiveRecord::Schema.define(version: 20170520133954) do
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
+  create_table "email_templates", force: :cascade do |t|
+    t.string "label"
+    t.string "subject"
+    t.string "body"
+    t.integer "author_id"
+    t.index ["author_id"], name: "index_email_templates_on_author_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "caption"
     t.bigint "user_id"
@@ -51,6 +48,19 @@ ActiveRecord::Schema.define(version: 20170520133954) do
     t.integer "photo_file_size"
     t.datetime "photo_updated_at"
     t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "sent_emails", force: :cascade do |t|
+    t.bigint "sender_id", null: false
+    t.bigint "recipient_id", null: false
+    t.datetime "time_sent"
+    t.string "reply_to_name"
+    t.string "reply_to_email"
+    t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["recipient_id"], name: "index_sent_emails_on_recipient_id"
+    t.index ["sender_id"], name: "index_sent_emails_on_sender_id"
   end
 
   create_table "taggings", id: :serial, force: :cascade do |t|
@@ -120,4 +130,6 @@ ActiveRecord::Schema.define(version: 20170520133954) do
   end
 
   add_foreign_key "posts", "users"
+  add_foreign_key "sent_emails", "users", column: "recipient_id"
+  add_foreign_key "sent_emails", "users", column: "sender_id"
 end
