a18eb4f23815bb48f6ce3d984777223457157dbf
Michael Byrd <michaelbyrdsemail@gmail.com>
2017-05-21 12:05:49 -0400

    patch

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   get "/profile/:id", to: "users#show", as: 'user_profile'
   get "/profile/:user_id/edit", to: "users#edit", as: 'edit_user_profile'
-  post "/profile/:user_id/update", to: "users#update", as: 'update_user_profile'
+  patch "/profile/:user_id/update", to: "users#update", as: 'update_user_profile'
   get "/profiles", to: "users#index", as: 'user_profiles'
   get "/grapevine", to: "users#index", as: 'grapevine'
 
