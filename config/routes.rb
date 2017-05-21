d91f601bf509431015a270a84227cc7f77bccc8e
Miranda Wang <msw3bb@virginia.edu>
2017-05-21 01:46:23 -0400

    Fixing social media buttons css problems

--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,14 @@ Rails.application.routes.draw do
   root to: "home#index"
   resources :email_templates
 
-  get "/profile/:user_id", to: "users#show", as: 'user_profile'
-  get "/profile/:user_id/edit", to: "users#edit", as: 'edit_user_profile'
-  patch "/profile/:user_id/update", to: "users#update", as: 'update_user_profile'
+  get "/profile/:id", to: "users#show", as: 'user_profile'
+  get "/profile/:id/edit", to: "users#edit", as: 'edit_user_profile'
+  post "/profile/:id/update", to: "users#update", as: 'update_user_profile'
   get "/profiles", to: "users#index", as: 'user_profiles'
+  get "/grapevine", to: "users#index", as: 'grapevine'
 
-  get   "/users/:user_id/verify", to: "verification#edit"
-  patch "/users/:user_id/verify", to: "verification#update", as: "verification"
-
-  get "/grapevine/", to: "users#index", as: 'grapevine'
+  get   "/users/:id/verify", to: "verification#edit"
+  patch "/users/:id/verify", to: "verification#update", as: "verification"
 
   post '/posts/:post_id/comment', to: 'posts#comment', as: 'comment_post'
 
