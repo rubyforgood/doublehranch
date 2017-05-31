b9abc770067d4f0cf21d6ff25c4caed4b008e05c
Adina Bianchi <adina.r.bianchi@gmail.com>
2017-05-30 20:15:16 -0400

    Add hide column to Posts table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170520183517) do
+ActiveRecord::Schema.define(version: 20170531000849) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,7 @@ ActiveRecord::Schema.define(version: 20170520183517) do
     t.string "photo_content_type"
     t.integer "photo_file_size"
     t.datetime "photo_updated_at"
+    t.boolean "hide", default: false, null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
