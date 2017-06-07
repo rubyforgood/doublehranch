e0a0293b380d6af967d47c46ff690ea6df8e975b
Adina Bianchi <adina.r.bianchi@gmail.com>
2017-06-07 19:54:54 -0400

    Clean up unrelated changes.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,28 +26,11 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:user_id])
 
-    privacy_settings_params.each do |setting|
-        @user.update(privacy_settings: setting)
-    end
-
     if @user.update_attributes(user_params)
       redirect_to user_profile_path(@user), notice: 'User updated.'
     else
       render :edit, alert: 'Unable to update user.'
     end
-  end
-  
-  def update_privacy_settings
-    @user = User.find(params[:user_id])
-
-    privacy_settings_params.each do |setting|
-        @user.update(privacy_settings: setting)
-    end
-
-      redirect_to user_profile_path(@user), notice: 'User updated.'
-    # else
-    #   render :edit, alert: 'Unable to update user.'
-    # end
   end
 
   def import
@@ -94,10 +77,6 @@ class UsersController < ApplicationController
     :twitter_handle,
     :zip_code,
     )
-  end
-
-  def privacy_settings_params
-    params.require(:user).permit(privacy_settings: {})
   end
 
 end
