819fff00d7b51b1f9152bd0dcc0f07aaf2166d04
maebeale <maebeale@gmail.com>
2017-09-30 12:17:13 -0400

    WIP: creating non-rails-admin way to create a user on the fly

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,14 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up,
+                                      keys: [:salutation,
+                                             :first_name,
+                                             :last_name,
+                                             :admin
+                                      ])
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,10 +26,28 @@ class UsersController < ApplicationController
     @posts = Post.where(user_id: params[:id])
   end
 
+  def new
+    @user = User.new
+  end
+
   def edit
     @user = User.find(params[:user_id])
     ['personal', 'social', 'contact', 'camp'].each do |key|
         @user.privacy_settings[key] ||= {}
+    end
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        format.json { render :show, status: :created, location: @user }
+      else
+        format.html { render :new }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
     end
   end
 
@@ -40,6 +58,14 @@ class UsersController < ApplicationController
       redirect_to user_profile_path(@user), notice: 'User updated.'
     else
       render :edit, alert: 'Unable to update user.'
+    end
+  end
+
+  def destroy
+    @user.destroy
+    respond_to do |format|
+      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
+      format.json { head :no_content }
     end
   end
 
