73fef89f32b024da0b5c993734fd80c01226bc09
maebeale <maebeale@gmail.com>
2017-12-11 16:51:09 -0500

    Include RequestSpecHelper in rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -65,6 +65,8 @@ RSpec.configure do |config|
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include Devise::Test::IntegrationHelpers, type: :feature
   config.include Warden::Test::Helpers
+  config.include RequestSpecHelper, type: :request
+
 
   config.after :each do
     Warden.test_reset!
