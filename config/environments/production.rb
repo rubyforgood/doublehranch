c27ea28152117a56342c3fdadcfa9478f529e6ae
Michael Byrd <michaelbyrdsemail@gmail.com>
2017-12-17 18:22:05 -0500

    adds default smtp setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -102,6 +102,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { :host => 'doublehranch.herokuapp.com' }
 
 end
