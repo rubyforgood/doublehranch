c241fb358966e97f7ae7a43e6cd5e880304afc12
Michael Byrd <michaelbyrdsemail@gmail.com>
2017-12-17 18:45:50 -0500

    adds SendGrid setup for emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -102,7 +102,21 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { :host => 'doublehranch.herokuapp.com' }
 
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.sendgrid.net',
+    port:                 587,
+    domain:               'doublehranch.herokuapp.com',
+    user_name:            ENV['SENDGRID_USERNAME'],
+    password:             ENV['SENDGRID_PASSWORD'],
+    authentication:       'plain',
+    enable_starttls_auto: true,
+    openssl_verify_mode:  'none'
+  }
 end
